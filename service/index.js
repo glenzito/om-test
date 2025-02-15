fa60f48333faf19bc5eb53026205a18338ff0ffe
Glen Mudau <44672521+glenzito@users.noreply.github.com>
2025-02-15 08:14:56 +0200

    start project

---
+++ service/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+
+
+const app = express()
+const port = 3000
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+  })
+  
